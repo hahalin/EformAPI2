7c1f457667911cb9db79980ddf7bebc4f473cf1f
FrankLin <frank.ch.lin@benq.com>
2016-09-28 19:21:47 +0800

    first import

---
+++ EformAPI2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EformAPI2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ EformAPI2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EformAPI2.WebApiApplication" Language="C#" %>
